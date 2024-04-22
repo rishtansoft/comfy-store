5d8b4c2d39db48264e01f1a7315e18302ac54825
rishtansoft <rishtansoft@gmail.com>
2024-04-22 17:07:14 +0500

    Router architecture, pages, layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Details from './pages/Details';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import ErrorPage from './pages/ErrorPage';
+import Checkout from './pages/Checkout';
+import Orders from './pages/Orders';
+import Layout from './layout';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate();
+
+  function ProtectedRoute({
+    children,
+    isAuthentication,
+    redirectTo = '/login',
+  }) {
+    if (!isAuthentication) {
+      navigate(redirectTo);
+    }
+
+    return children;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Routes>
+        {/* public  */}
+        <Route
+          path='/'
+          element={
+            <Layout>
+              <Home></Home>
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path='/about'
+          element={
+            <Layout>
+              <About></About>
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path='/products'
+          element={
+            <Layout>
+              <Products></Products>
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path='/cart'
+          element={
+            <Layout>
+              <Cart></Cart>
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path='/product/:id'
+          element={
+            <Layout>
+              <Details></Details>
+            </Layout>
+          }
+        ></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<ErrorPage></ErrorPage>}></Route>
+
+        {/* protected */}
+        <Route
+          path='/checkout'
+          element={
+            <ProtectedRoute isAuthentication={false}>
+              <Layout>
+                <Checkout></Checkout>
+              </Layout>
+            </ProtectedRoute>
+          }
+        ></Route>
+
+        <Route
+          path='/orders'
+          element={
+            <ProtectedRoute isAuthentication={false}>
+              <Layout>
+                <Orders></Orders>
+              </Layout>
+            </ProtectedRoute>
+          }
+        ></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/layout/index.jsx
@@ -0,0 +1,10 @@
+function Layout({children}) {
+  return (
+    <div>
+     <header>Header section</header>
+     {children}
+    </div>
+  )
+}
+
+export default Layout
