6be364bd6ef1029753deddb429e94a233db8bdb2
rishtansoft <rishtansoft@gmail.com>
2024-04-23 22:02:14 +0500

    header cart mode and logo mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { ThemeContext } from '../App';
 function Header() {
-
   const theme = useContext(ThemeContext);
 
   return (
@@ -26,7 +25,7 @@ function Header() {
             <div className='navbar-start'>
               <Link
                 to='/'
-                className='btn btn-primary text-2xl hover:opacity-70'
+                className={`btn ${theme.theme == 'light' ? 'btn-primary' : 'btn-secondary'} text-2xl hover:opacity-70`}
               >
                 C
               </Link>
@@ -60,11 +59,11 @@ function Header() {
                   value='synthwave'
                   onChange={(e) => {
                     if (e.target.checked) {
-                      theme.setTheme('dark')
-                      localStorage.setItem('theme', 'dark')
+                      theme.setTheme('dark');
+                      localStorage.setItem('theme', 'dark');
                     } else {
-                      theme.setTheme('light')
-                      localStorage.setItem('theme', 'light')
+                      theme.setTheme('light');
+                      localStorage.setItem('theme', 'light');
                     }
                   }}
                 />
@@ -85,6 +84,32 @@ function Header() {
                   <path d='M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z' />
                 </svg>
               </label>
+
+              <Link to = '/cart'>
+                <div
+                  tabIndex={0}
+                  role='button'
+                  className='btn btn-ghost btn-circle'
+                >
+                  <div className='indicator'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-6 w-6'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                      />
+                    </svg>
+                    <span className={`badge badge-sm indicator-item ${theme.theme == 'light' ? 'badge-primary' : 'badge-secondary'}`}>8</span>
+                  </div>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
